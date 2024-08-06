4ee6ae4337daa304878965bef97e5291e40cce8e
joe <j96263732@gmail.com>
2024-08-07 03:31:40 +0900

    deisgn : 메모리얼북 디자인 다시하는 중

--- everStarFrontend/src/components/atoms/images/Bookcover/Bookcover.tsx
+++ everStarFrontend/src/components/atoms/images/Bookcover/Bookcover.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as BookCoverSVG } from 'assets/images/book-cover.svg';
 
 export const AtomBookcover = () => {
   return (
-    <div className='relative w-[360px] h-[600px]'>
+    <div className='relative w-[360px] h-[508px]'>
       <BookCoverSVG className='w-full h-full' />
     </div>
   );
